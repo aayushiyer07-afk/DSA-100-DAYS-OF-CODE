3baccd5f5f8d50b57293cf00e8a7533e6284114e
aayushiyer07-afk <aayushiyer07@gmail.com>
2026-03-21 08:56:56 +0530

    Add files via upload

---
+++ day49.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definition of TreeNode
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+// Function to create a new node
+struct TreeNode* createNode(int val) {
+    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    newNode->val = val;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+// Insert into BST
+struct TreeNode* insertIntoBST(struct TreeNode* root, int val) {
+    if (root == NULL) {
+        return createNode(val);
+    }
+
+    if (val < root->val) {
+        root->left = insertIntoBST(root->left, val);
+    } else {
+        root->right = insertIntoBST(root->right, val);
+    }
+
+    return root;
+}
