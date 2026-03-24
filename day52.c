d8371e090929e2b9c5c2409f8f32d9fd11e6a1a2
aayushiyer07-afk <aayushiyer07@gmail.com>
2026-03-24 08:11:05 +0530

    Add files via upload

---
+++ day52.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+// Create new node
+struct TreeNode* newNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+// Build tree from level order
+struct TreeNode* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1) return NULL;
+
+    struct TreeNode* nodes[n];
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != -1)
+            nodes[i] = newNode(arr[i]);
+        else
+            nodes[i] = NULL;
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nodes[i] != NULL) {
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+
+            if (left < n)
+                nodes[i]->left = nodes[left];
+            if (right < n)
+                nodes[i]->right = nodes[right];
+        }
+    }
+
+    return nodes[0];
+}
+
+// LCA function
+struct TreeNode* LCA(struct TreeNode* root, int p, int q) {
+    if (root == NULL) return NULL;
+
+    if (root->val == p || root->val == q)
+        return root;
+
+    struct TreeNode* left = LCA(root->left, p, q);
+    struct TreeNode* right = LCA(root->right, p, q);
+
+    if (left != NULL && right != NULL)
+        return root;
+
+    return (left != NULL) ? left : right;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int p, q;
+    scanf("%d %d", &p, &q);
+
+    struct TreeNode* root = buildTree(arr, n);
+    struct TreeNode* ans = LCA(root, p, q);
+
+    if (ans != NULL)
+        printf("%d\n", ans->val);
+
+    return 0;
+}
